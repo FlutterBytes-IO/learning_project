c64252fe16a088e9b8e810d5c0790e93bcc2a2b3
Jamiu Okanlawon <developerjamiu@gmail.com>
2022-11-20 19:12:09 +0100

    chore: pushing what i have

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learning_project/app.dart';
+import 'package:learning_project/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
